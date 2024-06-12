fc3a276a9826cacd552625ee27e0e979cbd8f23a
Julia <jrhuliafy@gmail.com>
2024-06-12 15:43:56 -0300

    Terceiro commit

---
+++ src/conta/util/Cores.java
@@ -0,0 +1,16 @@
+package conta.util;
+
+public class Cores {
+
+	public static final String TEXT_WHITE_BOLD = null;
+	public static final int TEXT_YELLOW = 0;
+	public static final int ANSI_BLACK_BACKGROUND = 0;
+	public static final String TEXT_RESET = null;
+	public static final String TEXT_RED_BOLD = null;
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
